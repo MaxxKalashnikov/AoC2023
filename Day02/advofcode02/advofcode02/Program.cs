593c6718bb9f9125326f89cf357d920641fd976e
MaxxKalashnikov <max.vanholl@mail.ru>
2023-12-01 22:43:43 +0200

    Added a folder for the second day

---
+++ Day02/advofcode02/advofcode02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advofcode02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
